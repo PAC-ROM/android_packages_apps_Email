abd3858d1cfca3352d56d999c29bf6087d3eeb35
yingying <yingying@codeaurora.org>
2013-12-26 14:36:59 +0800

    Email: Support Email could configure the sync size function.
     - Make the user could enable the configure the sync size function to save
       the data. And the user could select the sync size for each account.
     - Add configure option in account settings interface.
    
    Change-Id: Ic043cb67afd249263cd480a003e854ac6efe4344

--- src/com/android/email/service/EmailServiceUtils.java
+++ src/com/android/email/service/EmailServiceUtils.java
@@ -640,6 +640,10 @@ public class EmailServiceUtils {
         }
 
         @Override
+        public void loadMore(long messageId) throws RemoteException {
+        }
+
+        @Override
         public void loadAttachment(final IEmailServiceCallback cb, final long accountId,
                 final long attachmentId, final boolean background) throws RemoteException {
         }
